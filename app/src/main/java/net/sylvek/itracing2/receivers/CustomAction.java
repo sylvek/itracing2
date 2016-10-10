801c7d2662dbd2fda2b6151bb42d0a8cf1afaa22
Sylvain Maucourt <sylvain.maucourt@deveryware.net>
2016-10-10 10:13:00 +0200

    fixes #80

--- app/src/main/java/net/sylvek/itracing2/receivers/CustomAction.java
+++ app/src/main/java/net/sylvek/itracing2/receivers/CustomAction.java
@@ -27,11 +27,11 @@ public class CustomAction extends BroadcastReceiver {
         final String action = Preferences.getCustomAction(context, address, source);
 
         if (action.startsWith("http://")) {
-            new CallUrl<>(action + "?address=" + address + "&source=" + source).start();
+            new CallUrl<>(action, "address=" + address + "&source=" + source).start();
         }
 
         if (action.startsWith("https://")) {
-            new CallUrl<HttpsURLConnection>(action + "?address=" + address + "&source=" + source).start();
+            new CallUrl<HttpsURLConnection>(action, "address=" + address + "&source=" + source).start();
         }
 
         if (!action.isEmpty()) {
@@ -43,9 +43,9 @@ public class CustomAction extends BroadcastReceiver {
 
         final String url;
 
-        public CallUrl(String url)
+        public CallUrl(String domain, String action)
         {
-            this.url = url;
+            this.url = domain + (domain.contains("?") ? "&" : "?") + action;
         }
 
         @Override
